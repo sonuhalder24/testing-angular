6bc73a4a106c03aa45123e2135283b1aceca1027
Dipa Halder <66179464+sonuhalder24@users.noreply.github.com>
2025-11-01 21:41:30 +0530

    Implement ApiService for HTTP requests

---
+++ api.ts
@@ -0,0 +1,87 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Credentials } from '../models/credentials.model';
+import { Users } from '../models/users.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  API_URL: string;
+  AUTH_API_URL = '/auth/server/';
+
+  constructor(private http: HttpClient) {
+    this.API_URL = 'http://localhost:5000/api';
+  }
+
+  public checkLogin(username: string, password: string): Observable<Credentials> {
+    return this.http.post<Credentials>(`${this.API_URL}${this.AUTH_API_URL}`, {
+      username,
+      password
+    }).pipe(catchError(this.handleError));
+  }
+
+  public getUserDetails(userId: number): Observable<Users> {
+    return this.http.get<Users>(`${this.API_URL}/users/${userId}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  public updateDetails(userDetails: Users): Observable<Users> {
+    return this.http.put<Users>(`${this.API_URL}/users/${userDetails.userId}`, userDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  public registerPatient(patientDetails: any): Observable<any> {
+    return this.http.post<any>(`${this.API_URL}/allpatients`, patientDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  public getAllPatientsList(): Observable<any> {
+    return this.http.get<any>(`${this.API_URL}/allpatients`)
+      .pipe(catchError(this.handleError));
+  }
+
+  public getParticularPatient(patientId: any): Observable<any> {
+    return this.http.get<any>(`${this.API_URL}/allpatients/${patientId}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  public getDiseasesList(): Observable<any> {
+    return this.http.get<any>(`${this.API_URL}/diseases`)
+      .pipe(catchError(this.handleError));
+  }
+
+  public bookAppointment(appointmentDetails: any): Observable<any> {
+    return this.http.post<any>(`${this.API_URL}/reqappointments`, appointmentDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  public requestedAppointments(): Observable<any> {
+    return this.http.get<any>(`${this.API_URL}/reqappointments`)
+      .pipe(catchError(this.handleError));
+  }
+
+  public getAppointments(patientId: any): Observable<any> {
+    return this.http.get<any>(`${this.API_URL}/reqappointments?patientId=${patientId}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  public deleteAppointment(appointmentId: any): Observable<any> {
+    return this.http.delete<any>(`${this.API_URL}/reqappointments/${appointmentId}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(() => error);
+  }
+}
